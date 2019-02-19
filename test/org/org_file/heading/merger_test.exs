aa4025a3c329fe1100e0c4d02da44c9d3674335a
HoffsMH <hecker.matthew@gmail.com>
2019-02-19 15:04:33 +0700

    added merger tests

---
+++ test/org/org_file/heading/merger_test.exs
@@ -0,0 +1,15 @@
+defmodule Org.OrgFile.Heading.MergerTest do
+  use ExUnit.Case
+  alias Org.OrgFile.Heading.Merger
+  alias Org.OrgFile.Heading
+
+  test "merge/2" do
+    with heading_one <- %Heading{value: "hi", content: ["content1"]},
+         heading_two <- %Heading{value: "hi", content: ["content2"]},
+         result <- Merger.merge(heading_one, heading_two) do
+
+      assert Map.get(result, :value) === "hi"
+      assert Map.get(result, :content) === ["content1", "content2"]
+    end
+  end
+end
