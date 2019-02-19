f69e015c4d0ac295914dcd292380522e7c393277
HoffsMH <hecker.matthew@gmail.com>
2019-02-19 12:58:53 +0700

    test for prepend on multiple files

--- test/org/sub_tasks/file_test.exs
+++ test/org/sub_tasks/file_test.exs
@@ -3,7 +3,7 @@ defmodule Org.FileTest do
   alias Org.File
   alias FS.IOMap
 
-  test "prepend/2" do
+  test "prepend/2 with a single file" do
     with args <- ["prefix-", "/some/file/name.exs"],
          io_map <- %IOMap{},
          resulting_io_map <- Org.File.prepend(io_map, args),
@@ -12,6 +12,23 @@ defmodule Org.FileTest do
          destination <- elem(Enum.at(renames, 0), 1) do
       assert source === "/some/file/name.exs"
       assert destination === "/some/file/prefix-name.exs"
+    end
+  end
+
+  test "prepend/2 with a multiple files" do
+    with args <- ["prefix-", "/some/file/name.exs", "/some/file/name\ other"],
+         io_map <- %IOMap{},
+         resulting_io_map <- Org.File.prepend(io_map, args),
+         renames <- get_in(Map.from_struct(resulting_io_map), [:actions, :rename]),
+         source_one <- elem(Enum.at(renames, 1), 0),
+         destination_one <- elem(Enum.at(renames, 1), 1),
+         source_two <- elem(Enum.at(renames, 0), 0),
+         destination_two <- elem(Enum.at(renames, 0), 1) do
+      assert source_one === "/some/file/name.exs"
+      assert destination_one === "/some/file/prefix-name.exs"
+
+      assert source_two === "/some/file/name other"
+      assert destination_two === "/some/file/prefix-name other"
     end
   end
 end
