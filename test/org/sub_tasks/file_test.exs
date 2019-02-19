fc5ce6b8e9fd631ada22864e8f25953c4a599ccc
HoffsMH <hecker.matthew@gmail.com>
2019-02-19 12:50:31 +0700

    added tests for file prepend simple case

--- test/org/sub_tasks/file_test.exs
+++ test/org/sub_tasks/file_test.exs
@@ -4,9 +4,14 @@ defmodule Org.FileTest do
   alias FS.IOMap
 
   test "prepend/2" do
-    with args <- ["prefix-", "~/some/file/name.exs"],
-         io_map <- %IOMap{} do
-      IO.inspect(Org.File.prepend(io_map, args))
+    with args <- ["prefix-", "/some/file/name.exs"],
+         io_map <- %IOMap{},
+         resulting_io_map <- Org.File.prepend(io_map, args),
+         renames <- get_in(Map.from_struct(resulting_io_map), [:actions, :rename]),
+         source <- elem(Enum.at(renames, 0), 0),
+         destination <- elem(Enum.at(renames, 0), 1) do
+      assert source === "/some/file/name.exs"
+      assert destination === "/some/file/prefix-name.exs"
     end
   end
 end
