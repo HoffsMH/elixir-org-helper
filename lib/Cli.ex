049617019fd69995e58d353f758a0cd9ed07089f
HoffsMH <hecker.matthew@gmail.com>
2019-02-24 16:29:25 +0700

    Basic structure of how cli calls the main app now

--- lib/Cli.ex
+++ lib/Cli.ex
@@ -8,21 +8,16 @@ defmodule Org.CLI do
 
   @default_root_dir "~/personal"
 
-  @default_file_args %{
+  @default_file_opts %{
     project_list_file: "#{@default_root_dir}/01-schedule/project-list.org",
     project_support_dir: "#{@default_root_dir}/02-project-support",
     capture_file: "#{@default_root_dir}/00-capture/capture.org"
   }
 
   def main(args) do
-    with file_args <- gen_file_args(args),
-         io_map <- FS.IOMap.gen_new(file_args),
+    with io_map <- FS.add_files(%FS.IOMap{}, @default_file_opts),
          new_io_map <- Org.run(io_map, args) do
       FS.apply_io!(new_io_map)
     end
-  end
-
-  def gen_file_args(_) do
-    @default_file_args
   end
 end
